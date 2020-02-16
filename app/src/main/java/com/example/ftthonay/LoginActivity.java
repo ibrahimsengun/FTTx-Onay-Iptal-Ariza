c89e1e122e3063c39ca4aaa5050324da47c96990
ibrahimsengun <ibrahim.sengun72@erzurum.edu.tr>
2020-02-16 23:02:46 +0300

    first commit

--- app/src/main/java/com/example/ftthonay/LoginActivity.java
+++ app/src/main/java/com/example/ftthonay/LoginActivity.java
@@ -8,16 +8,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
